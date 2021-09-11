53196bfe9c139bf69d9b690ce9106605136cad2a
pooja <gundagepooja@gmail.com>
2021-09-11 12:36:34 +0530

    to check conflict test4

---
+++ src/test4.java
@@ -0,0 +1,14 @@
+
+public class test3{
+private void syso() {
+	System.out.println("Hello");
+	System.out.println("Hello");
+	System.out.println("44444");
+
+}
+
+public static void main(String[] args) {
+	test2 t =new test2();
+	t.syso();
+}
+}
